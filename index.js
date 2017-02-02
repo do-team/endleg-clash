aa9518762363a40095359c03f5e37378045e92d5
kubedav <davesade.42@gmail.com>
2017-02-02 15:46:38 +0100

    FIRST!

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ console.log('Loading function');
 exports.handler = (event, context, callback) => {
     //console.log('Received event:', JSON.stringify(event, null, 2));
     console.log(event);
-
+    console.log(event.kinesis);
     callback(null, event);  // Echo back the first key value
     //callback('Something went wrong');
 };
