7d95af79424d8226c162fdda205f753bd736ec5e
David Kubec <davesade.42@gmail.com>
2017-02-03 10:40:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ console.log('Loading function');
 exports.handler = function(event, context) {
     console.log(JSON.stringify(event, null, 2));
     event.Records.forEach(function(record) {
-        // Kinesis data is base64 encoded so decode here
+        // Kinesis data is base64 encoded so decode here BLABLABLA
         payload = new Buffer(record.kinesis.data, 'base64').toString('ascii');
         console.log('Decoded payload:', payload);
     });
