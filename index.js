83bd4116a3d627058ac528df7434f48bb1e23173
David Kubec <davesade.42@gmail.co,m>
2017-02-22 07:42:50 +0100

    Update parameters added.

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 var AWS = require("aws-sdk");
 
 exports.handler = function(event, context) {
-    //console.log('Pure event:', event);
-    //console.log('Event Records:', event.Records);
-    //console.log('Event Records SNS:', event.Records[0].Sns);
-    //console.log('Event Records SNS Message:', event.Records[0].Sns.Message); // This contains exactly the message we are sending by batch.
-
+    // Parsing incoming object from SNS, result is incoming array with two users and theird cards.
     var incoming = JSON.parse(event.Records[0].Sns.Message);
 
     console.log('Player one >>> ', incoming[0].user, ' <<<   VS   Player two >>> ', incoming[1].user + ' <<< ');
-
+    // Definitions of winning conditions
     var combinations = {rock : {name: "rock", defeats: ["scissors","lizard"]},
                      paper: {name: "paper", defeats: ["rock", "spock"]},
                      scissors: {name: "scissors", defeats: ["paper", "lizard"]},
@@ -23,11 +19,9 @@ exports.handler = function(event, context) {
     var p2DrawScore = 0;
     var p1LoseScore = 0;
     var p2LoseScore = 0;
-
+    // Main loop to cycle all cards, to find a winner
     for (battle = 1; battle < 6; battle++) {
         card = ["card" + battle];
-
-
         console.log(incoming[0][card], incoming[1][card]);
         if(incoming[0][card] === incoming[1][card]){
             console.log("TIE! Round: ", battle);
@@ -42,7 +36,7 @@ exports.handler = function(event, context) {
                 console.log("Player ", incoming[0].user, " defeats ", incoming[1].user, " (", incoming[0][card], " beats ", incoming[1][card] ,")." );
                 p1WinScore++;
                 p2LoseScore++;
-            }else{
+            } else {
                 console.log("Player ", incoming[1].user, " defeats ", incoming[0].user, " (", incoming[1][card], " beats ", incoming[0][card] ,")." );
                 p1LoseScore++;
                 p2WinScore++;
@@ -51,80 +45,42 @@ exports.handler = function(event, context) {
     }
 };
 
-
 /*
-    //Improved check, inspired by Mke Spa Guy
-    var victory = userChoice.defeats.indexOf(computerChoice) > -1;
+// Callback function to store data into dynamo.
+for loop user0 user1
+updateUserParams Table = endleg-main
+                Key: user[0]
+                Update: fightflag = 0
 
-    //Display result
-    if(victory) {
-        alert("Vitory! " + userChoice.name + " wins!")
-    }else{
-        alert("Defeat, " + computerChoice + " wins!");
+updateScoreParams Table = endleg-score
+                Key: user
+                Update: Wins
+                        Draws
+                        Lose
+                        History = []
+                        LastCombination
+
+var params = {
+    TableName:table,
+    Key:{
+        "year": year,
+        "title": title
+    },
+    UpdateExpression: "remove info.actors[0]",
+    ConditionExpression: "size(info.actors) > :num",
+    ExpressionAttributeValues:{
+        ":num":3
+    },
+    ReturnValues:"UPDATED_NEW"
+};
+
+console.log("Attempting a conditional update...");
+docClient.update(params, function(err, data) {
+    if (err) {
+        console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
     }
-}
-
-*/
-
-
-
-/*
-
-
-db user update: key array.0.user.fighflag = 0
-db user update: key array.1.user.fighflag = 0
-
-db score update key array.0.user {
-    wins
-    draws
-    lose
-    history [array.0.card.1 + array.0.card.2 + array.0.card.3 + array.0.card.4 + array.0.card.5 VS array.1.card.1 + array.1.card.2 + array.1.card.3 + array.1.card.4 + array.1.card.5]
-}
-
-db score update key array.1.user {
-    wins
-    draws
-    lose
-    history [array.1.card.1 + array.1.card.2 + array.1.card.3 + array.1.card.4 + array.1.card.5 VS array.0.card.1 + array.0.card.2 + array.0.card.3 + array.0.card.4 + array.0.card.5]
-}
-
-
-*/
-
-
-/*
-
-//Check for a tie
-if(computerChoice == userChoice){
-    alert("It's a tie");
-//Check for a valid choice
-}else if(choices[userChoice] === undefined){
-    alert("Invalid Choice");
-}else{
-    //Get the chosen one as an object
-    userChoice = choices[userChoice];
-
-
-
-    //Check For a win
-    /*var victory = false;
-    for(var i = 0; i < userChoice.defeats.length; i++){
-        if(computerChoice == userChoice.defeats[i])
-        {
-            victory = true;
-            break;
-        }
-    }*/
-/*
-    //Improved check, inspired by Mke Spa Guy
-    var victory = userChoice.defeats.indexOf(computerChoice) > -1;
-
-    //Display result
-    if(victory) {
-        alert("Vitory! " + userChoice.name + " wins!")
-    }else{
-        alert("Defeat, " + computerChoice + " wins!");
-    }
-}
+});
 
 */
